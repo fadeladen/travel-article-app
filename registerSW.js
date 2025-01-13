310ec8c302961b53be99b02e9ce9921b78851d41
fadeladen <fadel.alfayed@62teknologi.com>
2025-01-13 11:13:02 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/travel-article-app/sw.js', { scope: '/travel-article-app/' })})}
